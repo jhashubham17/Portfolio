82b9471e9ea23cb70a15a022b78e6dd80d2742c5
Shubham jha <sjha870054@gmail.com>
2025-03-26 09:42:58 +0530

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -80,7 +80,7 @@ const Hero = () => {
               View Certificates
             </motion.a>
             <motion.a
-              href="https://drive.google.com/file/d/1keIurNJ3XJnBDqIqKCphexXdKBYgPVg7/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1XGAdKHwrMemdun2MIsJQ6TSUY1WuC_Zo/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="inline-block bg-transparent border-2 border-blue-400 text-blue-400 px-8 py-4 rounded-full font-semibold text-lg hover:bg-blue-400/10 transition-all duration-300"
